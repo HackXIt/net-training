23da63a76dd8dda5989be7509b08f98aa4a9a008
Nikolaus Rieder <n.rieder@schrack-seconet.com>
2022-07-21 14:57:08 +0200

    Completed MVVM_Tutorial #2

---
+++ MVVM_Tutorial/MVVM_2/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MVVM_2
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ MVVM_Tutorial/MVVM_2/MainWindowViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace MVVM_2
+{
+    public class MainWindowViewModel
+    {
+        public MainWindowViewModel()
+        {
+            Firstname = "Dev";
+            Lastname = "Nick";
+        }
+        
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+
+        public string Fullname => $"{Firstname} {Lastname}";
+    }
+}
