e2e91d842822a7df23d77eb71332e3aa1006e68d
Nikolaus Rieder <n.rieder@schrack-seconet.com>
2022-07-26 14:24:20 +0200

    Finished MVVM_Tutorial #8 (Was relatively short)

---
+++ MVVM_Tutorial/MVVM_8/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace MVVM_8
+{
+    public class Person
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Fullname => $"{Firstname} {Lastname}";
+        public string Department { get; set; }
+    }
+}
