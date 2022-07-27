707b369ed29ee79e3f4460b585f6938114be96d4
Nikolaus Rieder <n.rieder@schrack-seconet.com>
2022-07-27 10:47:18 +0200

    Finished MVVM_Tutorial #9

---
+++ MVVM_Tutorial/MVVM_9/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace MVVM_9
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
