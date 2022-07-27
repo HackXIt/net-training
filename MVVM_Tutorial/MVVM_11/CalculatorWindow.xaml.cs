72f8d5da7f23c47e1f2d59e279c458d29dabb40c
Nikolaus Rieder <n.rieder@schrack-seconet.com>
2022-07-27 14:15:18 +0200

    Finished MVVM_Tutorial #11

---
+++ MVVM_Tutorial/MVVM_11/CalculatorWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace MVVM_11
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class CalculatorWindow
+    {
+        public CalculatorWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MVVM_Tutorial/MVVM_11/NotifiableBaseObject.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace MVVM_11
+{
+    public class NotifiableBaseObject : INotifyPropertyChanged
+    {
+        // Zusatz - War nicht im Tutorial enthalten
+        // https://stackoverflow.com/questions/1196991/get-property-value-from-string-using-reflection
+        // Generic Property for accessing property values using reflection
+        // Details über den Typ dieser Property findet man hier: https://docs.microsoft.com/de-de/dotnet/csharp/programming-guide/indexers/
+        private object this[string propertyName] => this.GetType().GetProperty(propertyName)?.GetValue(this, null);
+
+        // EventHandler vom Interface INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // CallerMemberName ist ein CompilerService und übergibt automatisch den Namen des Members, welcher die Methode aufruft
+        // Somit benötigt man keine Methoden-Überladung 
+        protected virtual void RaisePropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            if (string.IsNullOrEmpty(propertyName)) return;
+            // Zusatz - war nicht im Tutorial enthalten
+            // Check out this: https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/tokens/verbatim
+            // In regards to the special character '@' => It interprets the string as verbatim, making that ReSharper warning go away
+            // Trace shows console output when running in Debugger
+            // Reflection is used to get the value of the calling attribute
+            Trace.WriteLine($"Changed {propertyName} to \"{this[propertyName]}\"");
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        
+    }
+}
