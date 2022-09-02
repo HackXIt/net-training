5c49b40c028659c8a6fd6b9806eecf6c7ed7d0c1
HackXIt <hackxit@gmail.com>
2022-09-02 15:42:06 +0200

    Add NLog package and logging functionality

--- DiscordBot/discord-training-bot/Program.cs
+++ DiscordBot/discord-training-bot/Program.cs
@@ -2,6 +2,9 @@
 
 using Discord;
 using Discord.WebSocket;
+using NLog;
+using NLog.Config;
+using NLog.Targets;
 
 namespace discord_training_bot;
 
@@ -10,15 +13,24 @@ public class Program
     public static Task Main(string[] args) => new Program().MainAsync();
 
     private DiscordSocketClient _client;
+    private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
 
     public async Task MainAsync()
     {
+        var logConfig = new LoggingConfiguration();
+        var logFile = new FileTarget("logfile") {FileName = "discord-training.log"};
+        var logConsole = new ConsoleTarget("logconsole");
+        logConfig.AddRule(LogLevel.Info, LogLevel.Fatal, logConsole);
+        logConfig.AddRule(LogLevel.Debug, LogLevel.Fatal, logFile);
+        LogManager.Configuration = logConfig;
+        
         _client = new DiscordSocketClient();
 
         _client.Log += Log;
 
         //  You can assign your bot token to a string, and pass that in to connect.
         //  This is, however, insecure, particularly if you plan to have your code hosted in a public repository.
+        // var token = "";
         var token = File.ReadAllText("token.txt");
 
         // Some alternative options would be to keep your token in an Environment Variable or a standalone file.
@@ -35,7 +47,7 @@ public class Program
     
     private Task Log(LogMessage msg)
     {
-        Console.WriteLine(msg.ToString());
+        Logger.Info(msg.ToString());
         return Task.CompletedTask;
     }
 }
